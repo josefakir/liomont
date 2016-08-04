b8c75dd6732d03e61b575053fd11538365b72def
José Becerra <antonio.becerra@grupomedios.com>
2016-08-04 11:02:32 -0500

    noticias

--- js/script.js
+++ js/script.js
@@ -8,25 +8,39 @@ function showLoading( on, params ) {  // on: true|false
       }       
     }, 1);
 }
-var urlwordpress = "https://www.dondeir.com/api/get_recent_posts/?page=1";
+var urlwordpress = "https://www.dondeir.com/api/";
 $( document ).on( "pageinit", "#iniciarsesion", function( event ) {
 });
 $( document ).on( "pageinit", "#noticias", function( event ) {
 	showLoading( true ); 
 	$.ajax({
-		url : urlwordpress,
+		url : urlwordpress+'get_recent_posts/?page=1',
 		success : function (data){
 			data = data.posts;
-			console.log(data.posts)
 			 $.each(data, function(i, item) {
-			 	console.log(item);
 			 	titulo = item.title;
 			 	extracto = item.excerpt;
 			 	imagen = item.attachments[0].url;
-			 	output = '<div class="noticia"><div class="imgnoticia"><img src="'+imagen+'" alt="Noticia"></div><div class="previonoticia"><h2>'+titulo+'</h2><br><div class="extractonoticia">'+extracto+'</div></div><div class="clear"></div></div>'
+			 	id = item.id;
+			 	url = item.url;
+			 	output = '<div class="noticia"><div class="imgnoticia"><a class="mostrarnota" rel="'+id+'"><img src="'+imagen+'" alt="Noticia"></a></div><div class="previonoticia"><h2><a class="mostrarnota" rel="'+id+'">'+titulo+'</a></h2><br><div class="extractonoticia">'+extracto+'</div></div><div class="clear"></div></div>'
 				$('#contenedornoticias').append(output);
 				showLoading( false ); 
 			});
 		}
+	});
+	$(document).on("click", ".mostrarnota", function() {
+		var id_post = $(this).attr('rel');
+		alert(id_post);
+		$.ajax({
+			url : urlwordpress+'get_post/?id='+id_post,
+			success : function(data){
+				titulo = data.post.title_plain;
+				imagen = data.post.attachments[0].url;
+				contenido = data.post.content;
+			 	$('#contenidonoticia').html('<h1>'+titulo+'</h1><div class="imageninterior"><img class="portadainterior" src="'+imagen+'"/></div><div class="contenidointerior">'+contenido+'</dv>');
+				$('#contenidonoticia').fadeIn('fast');
+			}
+		});
 	});
 });
