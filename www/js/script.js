0af99bce60f3b0840630ea718d53441f6dbe12aa
Pepe Becerra <pepe@Pepes-MacBook-Pro.local>
2016-08-21 11:37:53 -0500

    device ready

--- www/js/script.js
+++ www/js/script.js
@@ -10,42 +10,22 @@ function showLoading( on, params ) {  // on: true|false
       }       
     }, 1);
 }
-/* phonegap */
-var app = {
-    // Application Constructor
-    initialize: function() {
-        this.bindEvents();
-    },
-    // Bind Event Listeners
-    //
-    // Bind any events that are required on startup. Common events are:
-    // 'load', 'deviceready', 'offline', and 'online'.
-    bindEvents: function() {
-        document.addEventListener('deviceready', this.onDeviceReady, false);
-    },
-    // deviceready Event Handler
-    //
-    // The scope of 'this' is the event. In order to call the 'receivedEvent'
-    // function, we must explicitly call 'app.receivedEvent(...);'
-    onDeviceReady: function() {
-        app.receivedEvent('deviceready');
-    },
-    // Update DOM on a Received Event
-    receivedEvent: function(id) {
-        var parentElement = document.getElementById(id);
-        var listeningElement = parentElement.querySelector('.listening');
-        var receivedElement = parentElement.querySelector('.received');
-
-        listeningElement.setAttribute('style', 'display:none;');
-        receivedElement.setAttribute('style', 'display:block;');
-
-        alert('Received Event: ' + id);
-    }
-};
 
 $(document).ready(function(){
 	$('#cerrarmensajes').click(function(e){
 		e.preventDefault();
 		$('#layermensaje').fadeOut('fast');
 	})
+});
+$(document).on("pagecreate", "#index", function() {
+    var value = window.localStorage.getItem("session");
+    if (value != null) {
+        $.mobile.loading('show');
+            function explode(){
+            $('#triggersesion').trigger( "click" );
+        }
+    setTimeout(explode, 500);
+    //$.mobile.pushStateEnabled = false;
+    //$.mobile.hashListeningEnabled = false;
+    }
 });
